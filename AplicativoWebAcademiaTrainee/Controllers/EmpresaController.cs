ca6981e9f439251dda068abbf1670b5d7d7e3d76
Jordana Rosa <94566421+JordanaRosa@users.noreply.github.com>
2021-11-22 00:36:14 -0300

    Criei View, Model e Controller para a entidade "Empresa" contendo as propriedades: Código, Nome, Nome Fantasia e CNPJ. Linkei o menu para abrir a View.

---
+++ AplicativoWebAcademiaTrainee/Controllers/EmpresaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AplicativoWebAcademiaTrainee.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        private readonly ILogger<EmpresaController> _logger;
+
+        public EmpresaController(ILogger<EmpresaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new Models.EmpresaModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AplicativoWebAcademiaTrainee/Models/EmpresaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AplicativoWebAcademiaTrainee.Models
+{
+    public class EmpresaModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
